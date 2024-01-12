68048e4c1e5dd8ea5a9238571e926fa5f69b5bcf
@nakulgyll <nakulgyll@gmail.com>
2024-01-12 09:28:22 +0530

    test file number 2 added

---
+++ testing_jenkins.py
@@ -0,0 +1,9 @@
+
+def output_jenkins():
+    value = '[+] ---------------------> function called scuessfully from jenkins script '
+    return(value)
+
+
+
+if __name__=='__main__':
+    output_jenkins()
