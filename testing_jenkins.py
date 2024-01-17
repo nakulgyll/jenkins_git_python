e80706186d29a8a216a73f5c4a1ae0b7f40444bb
@nakulgyll <nakulgyll@gmail.com>
2024-01-17 13:32:11 +0530

    python file modified 4 json file creating

--- testing_jenkins.py
+++ testing_jenkins.py
@@ -4,17 +4,19 @@ import json
 
 
 def output_jenkins():
-    value = 'OOsucessOO'
-
-    x = {
-      "name": "John",
-      "age": 30,
-      "city": "New York"
+   dictionary = {
+    "name": "sathiyajith",
+    "rollno": 56,
+    "cgpa": 8.6,
+    "phonenumber": "9976770500"
     }
-
-    # convert into JSON:
-    y = json.dumps(x)
-    return(x)
+     
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
+     
+    # Writing to sample.json
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
 
 
 
